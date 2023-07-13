a9584cd1a9a9a634cf6ab320287c01eea07561d6
Alimova Julia <markiza1014@gmail.com>
2023-07-13 09:40:26 +0300

    end half work

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,51 @@ const router = express.Router()
 
 // ================================================================
 
+class Product {
+  static #list = []
+  constructor(name, price, description) {
+    this.id = Math.floor(Math.random() * 90000) + 10000
+    this.createDate = new Date().toISOString()
+    this.name = name
+    this.price = price
+    this.description = description
+  }
+
+  static getList = () => this.#list
+  static add = (product) => this.#list.push(product)
+  static getById = (id) =>
+    this.#list.find((product) => product.id === id)
+  static updateById = (
+    id,
+    { name, price, description },
+  ) => {
+    const product = this.getById(id)
+    if (product) {
+      if (name && price && description) {
+        product.name = name
+        product.price = price
+        product.description = description
+        return true
+      }
+    } else {
+      return false
+    }
+  }
+  static deleteById = (id) => {
+    const index = this.#list.findIndex(
+      (product) => product.id === id,
+    )
+    if (index !== -1) {
+      this.#list.splice(index, 1)
+      return true
+    } else {
+      return false
+    }
+  }
+}
+
+// ================================================================
+
 // router.get Створює нам один ентпоїнт
 
 // ↙️ тут вводимо шлях (PATH) до сторінки
@@ -17,6 +62,44 @@ router.get('/', function (req, res) {
     style: 'index',
   })
   // ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+router.get('/product-create', function (req, res) {
+  res.render('product-create', {
+    style: 'product-create',
+  })
+})
+
+// ================================================================
+
+router.post('/product-create', function (req, res) {
+  const { name, price, description } = req.body
+
+  const product = new Product(name, price, description)
+
+  Product.add(product)
+
+  res.render('alert', {
+    style: 'alert',
+    info: 'Success! Product created!',
+  })
+})
+
+// ================================================================
+
+router.get('/product-list', function (req, res) {
+  const list = Product.getList()
+
+  res.render('product-list', {
+    style: 'product-list',
+
+    products: {
+      list,
+      isEmpty: list.length === 0,
+    },
+  })
 })
 
 // ================================================================
